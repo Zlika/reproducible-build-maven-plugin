a4ca29d94bfedf755e86fefb8aee99e931bef27e
Thomas <zlika_ese@hotmail.com>
2015-10-02 21:22:43 +0200

    Add class to normalize ObjectFactory java files generated by xjc.

---
+++ src/test/resources/io/github/zlika/reproducible/WrongObjectFactory.java
@@ -0,0 +1,71 @@
+
+package test;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the test package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _Author_QNAME = new QName("", "author");
+    private final static QName _Book_QNAME = new QName("", "book");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: test
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Book }
+     * 
+     */
+    public Book createBook() {
+        return new Book();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Author }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "", name = "author")
+    public JAXBElement<Author> createAuthor(Author value) {
+        return new JAXBElement<Author>(_Author_QNAME, Author.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link Author }
+     * 
+     */
+    public Author createAuthor() {
+        return new Author();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Book }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "", name = "book")
+    public JAXBElement<Book> createBook(Book value) {
+        return new JAXBElement<Book>(_Book_QNAME, Book.class, null, value);
+    }
+
+}
