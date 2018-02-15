c0f5471184f4d28d0061d2b65d1df290a07ebbee
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2018-02-15 14:42:18 +0100

    unify tar IT in one

--- src/it/strip-tar/verify.groovy
+++ src/it/strip-tar/verify.groovy
@@ -1,6 +1,10 @@
 import java.nio.file.Files;
 
-// Check that both artifacts produced by the two builds are the same
-byte[] content1 = Files.readAllBytes(new File(basedir, "target1/same-jar-1.0-SNAPSHOT-tar.tar").toPath());
-byte[] content2 = Files.readAllBytes(new File(basedir, "target2/same-jar-1.0-SNAPSHOT-tar.tar").toPath());
-assert content1 == content2
+["same-jar-1.0-SNAPSHOT-tar.tar", "same-jar-1.0-SNAPSHOT-tar.tar.gz", "same-jar-1.0-SNAPSHOT-tar.tar.bz2"].each { file ->
+    // Check that both artifacts produced by the two builds are the same
+    byte[] content1 = Files.readAllBytes(new File(basedir, "target1/${file}").toPath());
+    byte[] content2 = Files.readAllBytes(new File(basedir, "target2/${file}").toPath());
+    assert content1 == content2
+}
+
+return true
