b06b031c8d2a6ebc999d466b49c949542d83b6be
Zlika <zlika_ese@hotmail.com>
2018-02-17 14:07:47 +0100

    Fix artifact name of strip-tar integration test

--- src/it/strip-tar/verify.groovy
+++ src/it/strip-tar/verify.groovy
@@ -1,7 +1,7 @@
 import java.nio.file.Files;
 
-["same-jar-1.0-SNAPSHOT-tar.tar", "same-jar-1.0-SNAPSHOT-tar.tar.gz", "same-jar-1.0-SNAPSHOT-tar.tar.bz2"].each { file ->
-    // Check that both artifacts produced by the two builds are the same
+["strip-tar-1.0-SNAPSHOT-tar.tar", "strip-tar-1.0-SNAPSHOT-tar.tar.gz", "strip-tar-1.0-SNAPSHOT-tar.tar.bz2"].each { file ->
+    // Check that both archives produced by the two builds are the same
     byte[] content1 = Files.readAllBytes(new File(basedir, "target1/${file}").toPath());
     byte[] content2 = Files.readAllBytes(new File(basedir, "target2/${file}").toPath());
     assert content1 == content2
